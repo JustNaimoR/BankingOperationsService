5fc3531227116d2c303655d670c9c87dffe3b93d
8bit_please <68927773+JustNaimoR@users.noreply.github.com>
2024-05-18 18:30:19 +0300

    initial commit

---
+++ src/main/java/effectiveMobile/demo/BankingOperationsServiceApplication.java
@@ -0,0 +1,13 @@
+package effectiveMobile.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankingOperationsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankingOperationsServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/effectiveMobile/demo/BankingOperationsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package effectiveMobile.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankingOperationsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
